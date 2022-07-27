01cab61bc80bdda287133132c527a618ca095047
Lorel <lorel.oboue@gmail.com>
2022-07-27 11:47:55 +0100

    usEffect code written

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import { useEffect, useState } from "react"
-import { Link, Route, Routes } from "react-router-dom"
-import ContactsList from "./components/ContactsList"
-import ContactsAdd from "./components/ContactsAdd"
-import ContactsView from "./components/ContactsView"
-import "./styles/styles.css"
+import { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import ContactsList from "./components/ContactsList";
+import ContactsAdd from "./components/ContactsAdd";
+import ContactsView from "./components/ContactsView";
+import "./styles/styles.css";
 
 export default function App() {
-  const [contacts, setContacts] = useState([])
-  
+  const [contacts, setContacts] = useState([]);
+
   //TODO: Load all contacts on useEffect when component first renders
+
+  // I will not need any dependencies,
+  // use fecth within useEffect and fetch the contacts data
+  // don't forget to log the data to see whats going on
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/contacts`)
+      .then((res) => res.json())
+
+      .then((data) => {
+        console.log("the data", data);
+        setContacts(data);
+      });
+  }, []);
+
+  // to get the list of contacts to show, create a link - for when it loads
 
   return (
     <>
@@ -16,15 +32,16 @@ export default function App() {
         <h2>Menu</h2>
         <ul>
           {/* TODO: Make these links */}
-          <li>Contacts List</li>
+          <Link to="/">Contacts List</Link>
+          {/* <li>Contacts List</li> */}
           <li>Add New Contact</li>
         </ul>
       </nav>
       <main>
         <Routes>
-          {/* TODO: Add routes here  */}
+          <Route path="/" />
         </Routes>
       </main>
     </>
-  )
+  );
 }
