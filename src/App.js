3a25e2f57b3581d13444126f4d936f3a1a74b4f4
Lorel <lorel.oboue@gmail.com>
2022-07-27 11:55:42 +0100

    route path added, contacts rendering

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ export default function App() {
       </nav>
       <main>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<ContactsList contacts={contacts} />} />
         </Routes>
       </main>
     </>
