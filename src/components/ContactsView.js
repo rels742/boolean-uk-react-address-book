6c79f30f55051529821fed30333fb72ed297f866
Lorel <lorel.oboue@gmail.com>
2022-07-28 16:53:49 +0100

    useparams added to ContactsView

--- src/components/ContactsView.js
+++ src/components/ContactsView.js
@@ -1,23 +1,40 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function ContactsView() {
-  const [contact, setContact] = useState(false)
+  const [contact, setContact] = useState(false);
 
   //TODO: Get the contact to load from the params and fetch.
   //With useEffect, load the contact when params changes
   //and update contact state
 
+  let { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/contacts/${id}`)
+      .then((res) => res.json())
+
+      .then((data) => {
+        setContact(data);
+      });
+  }, [id]);
+
+  //nts if fetch fails, check how I've coded the url - have I hardcoded instead of interpolated. Eg before the useEffect above was working, I wrote http://localhost:4000/contacts/:id instead of what is now above
+
   if (!contact) {
-    return <p>Loading</p>
+    return <p>Loading</p>;
   }
 
   return (
     <div>
-      <h2>{contact.firstName} {contact.lastName}</h2>
-      <p>{contact.street} {contact.city}</p>
+      <h2>
+        {contact.firstName} {contact.lastName}
+      </h2>
+      <p>
+        {contact.street} {contact.city}
+      </p>
     </div>
-  )
+  );
 }
 
-export default ContactsView
+export default ContactsView;
